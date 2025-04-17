21580eb1fb550c466943f6f680d673ee7149dd85
Avery Sui <averysui@outlook.com>
2025-04-17 09:15:46 +0800

    modify cors

--- server.js
+++ server.js
@@ -8,16 +8,19 @@ import paymentRoutes from "./routes/payment.js"
 import checkoutRouter from "./routes/checkout.js"
 import authRouter from "./routes/auth.js"
 import webhookRouter from "./routes/webhook.js"
-
 dotenv.config()
 
 const app = express()
 
+app.use(cors({
+  origin: 'https://crackcodeinterview.io', // 只允许这个前端来源
+  credentials: true,                      // 如果你要发送 cookie 或认证信息
+}))
+
 // 首先注册 webhook 路由，因为它需要原始请求体
 app.use("/api/webhook", webhookRouter)
 
-// 然后注册其他中间件
-app.use(cors())
+
 app.use(express.json({ limit: "10mb" }))
 
 // 最后注册其他路由
